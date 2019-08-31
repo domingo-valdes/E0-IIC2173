bd611d6ae0f4b243ec33a5716b4d2b92bcdae964
Domingo <dvaldes3@uc.cl>
2019-08-31 17:31:07 -0400

    pagination progress

--- chatting/views.py
+++ chatting/views.py
@@ -1,5 +1,8 @@
 from django.template import loader
 from django.http import HttpResponse
+from django.core.paginator import Paginator
+from django.shortcuts import render
+
 from .models import Message
 from .forms import MessageForm
 
@@ -8,12 +11,31 @@ def index(request):
     form = MessageForm(request.POST or None)
     if form.is_valid():
         form.save()
-    latest_message_list = Message.objects.order_by('-pub_date')[:5]
+    # latest_message_list = Message.objects.order_by('-pub_date')[:5]
+    latest_message_list = Message.objects.order_by('-id')[:100][::-1]
+    page = request.GET.get('page',1)
+    paginator = Paginator(latest_message_list, 5)
     template = loader.get_template('chatting/index.html')
     context = {
         'form': form,
-        'latest_message_list': latest_message_list,
+        'messages': paginator.page(page),
     }
-    return HttpResponse(template.render(context, request))
+    return render(request, 'chatting/index.html', context)
+    #return HttpResponse(template.render(context, request))
+
+
+# def index(request):
+#     user_list = User.objects.all()
+#     page = request.GET.get('page', 1)
+
+#     paginator = Paginator(user_list, 10)
+#     try:
+#         users = paginator.page(page)
+#     except PageNotAnInteger:
+#         users = paginator.page(1)
+#     except EmptyPage:
+#         users = paginator.page(paginator.num_pages)
+
+#     return render(request, 'core/user_list.html', { 'users': users })
 
 

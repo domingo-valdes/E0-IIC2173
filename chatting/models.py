7e6cae515d2977f5be545a472bf7b9bc7a5ac759
Domingo <dvaldes3@uc.cl>
2019-08-31 12:07:48 -0400

    chat working

--- chatting/models.py
+++ chatting/models.py
@@ -1,8 +1,9 @@
 from django.db import models
+from django.utils import timezone
 
 class Message(models.Model):
     message_text = models.CharField(max_length=500)
-    pub_date = models.DateTimeField('date published')
+    pub_date = models.DateTimeField(default=timezone.now)
     user_alias = models.CharField(max_length=100, default='Anonymous')
 
     def __str__(self):
--- chatting/views.py
+++ chatting/views.py
@@ -1,9 +1,19 @@
-from django.shortcuts import render
+from django.template import loader
+from django.http import HttpResponse
 from .models import Message
-
+from .forms import MessageForm
 
 
 def index(request):
+    form = MessageForm(request.POST or None)
+    if form.is_valid():
+        form.save()
     latest_message_list = Message.objects.order_by('-pub_date')[:5]
-    context = {'latest_message_list': latest_message_list}
-    return render(request, 'chatting/index.html', context)
+    template = loader.get_template('chatting/index.html')
+    context = {
+        'form': form,
+        'latest_message_list': latest_message_list,
+    }
+    return HttpResponse(template.render(context, request))
+
+
